47033b184a59eb24290e66f1b7e1c3e76becd25d
Yan <yancarlo1206@gmail.com>
2019-04-23 21:16:48 -0500

    Implementando productos relacionados

--- controllers/productoController.php
+++ controllers/productoController.php
@@ -4,6 +4,7 @@ class productoController extends Controller {
     public function __construct() {
         parent::__construct();
         $this->_producto = $this->loadModel('producto');
+        $this->_relacionado = $this->loadModel('relacionado');
     }
     
     public function index() { }
@@ -11,6 +12,7 @@ class productoController extends Controller {
     public function detalle($producto=null) {
         $this->_view->titulo = 'Producto Detalle';
         $this->_view->producto = $this->_producto->get($producto);
+        $this->_view->productoRelacionados = $this->_relacionado->findBy(array("producto" => $producto));
         $this->_view->renderizar('index', 'producto');
     }
 
