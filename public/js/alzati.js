8aad22a3b0d1deb16fcdb11e01a65dd28081db29
Yan Carlo Angarita Sanguino <y.angarita@unisimonbolivar.edu.co>
2019-04-30 07:59:48 -0500

    Recuperando vista de imagenes de vista rapida

--- public/js/alzati.js
+++ public/js/alzati.js
@@ -16,6 +16,13 @@ jQuery(document).ready(function($) {
 			$("#stock").html("<span>Habilitados:</span> "+data.stock+" en Bodega");
 			$("#nombre").html(data.nombre);
 			$("#precio").html(data.precio);
+			var url = BASE.url+"public/img/producto/";
+			var imgFrente = url+data.id+".jpg";
+			var imgDetras = url+data.id+"_1.jpg";
+			$("#imgFrente").attr('src',imgFrente);
+			$("#imgFrente").attr('data-src',imgFrente);
+			$("#imgDetras").attr('src',imgDetras);
+			$("#imgDetras").attr('data-src',imgDetras);
 			$("#anadir").attr("data-producto", data.id);
 			$("#cantidad").attr("data-id", data.id);
 			$("#anadir").attr("data-nombre", data.nombre);
