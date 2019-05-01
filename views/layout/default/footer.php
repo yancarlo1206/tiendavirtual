ae1bee6bcdd9fd8838cd6e17d6f089d390f3e5a3
Yan <yancarlo1206@gmail.com>
2019-05-01 17:29:51 -0500

    Redes sociales

--- views/layout/default/footer.php
+++ views/layout/default/footer.php
@@ -23,11 +23,12 @@
         </div>
         <div class="col-md-auto">
           <ul class="tt-social-icon">
-            <li><a class="icon-g-64" target="_blank" href="http://www.facebook.com/"></a></li>
+            <li><a>Instagram</a></li>
+            <!-- <li><a class="icon-g-64" target="_blank" href="http://www.facebook.com/"></a></li>
             <li><a class="icon-h-58" target="_blank" href="http://www.facebook.com/"></a></li>
-            <li><a class="icon-g-66" target="_blank" href="http://www.twitter.com/"></a></li>
-            <li><a class="icon-g-67" target="_blank" href="http://www.google.com/"></a></li>
-            <li><a class="icon-g-70" target="_blank" href="https://instagram.com/"></a></li>
+            <li><a class="icon-g-66" target="_blank" href="http://www.twitter.com/"></a></li 
+            <li><a class="icon-g-70" target="_blank" href="https://instagram.com/"></a></li> -->
+            <li><a class="icon-g-67" target="_blank" href="https://www.instagram.com/alzatishoes/"></a></li> 
           </ul>
         </div>
       </div>
